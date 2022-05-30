faa593f94e917a146f7aa8d4917d92331dd3f38f
Inchan kim <73270177+kiminchan@users.noreply.github.com>
2022-05-30 14:31:54 +0900

    🔨 edit App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./styles.css";
 
+const GundamHeader = () => {
+  return <h3>건담</h3>;
+};
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <GundamHeader />
+      <GundamHeader />
     </div>
   );
 }
