cf098bb3a7b408ee5e0fa0f705dc61b23fdc9788
Inchan kim <73270177+kiminchan@users.noreply.github.com>
2022-05-30 14:34:07 +0900

    🌱 add GundamTitle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./styles.css";
 
+const GundamTitle = () => {
+  return <h1>건담 스토리</h1>;
+};
+
 const GundamHeader = () => {
   return <h3>건담</h3>;
 };
@@ -7,7 +11,7 @@ const GundamHeader = () => {
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <GundamTitle />
       <GundamHeader />
       <GundamHeader />
     </div>
